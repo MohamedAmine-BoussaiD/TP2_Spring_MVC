f8a44cdd5775d010e03f3433ffc8623dba80d700
aminebsd <amineboussaid234@gmail.com>
2026-03-08 16:43:58 +0100

    first commit

---
+++ src/main/java/com/sdia/tp2/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sdia.tp2.repository;
+
+import com.sdia.tp2.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
