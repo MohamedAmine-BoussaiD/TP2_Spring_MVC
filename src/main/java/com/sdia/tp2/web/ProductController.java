22c56cff3022df779b66dc3f9262f94f313ed981
aminebsd <amineboussaid234@gmail.com>
2026-03-09 01:41:35 +0100

    spring mvc

--- src/main/java/com/sdia/tp2/web/ProductController.java
+++ src/main/java/com/sdia/tp2/web/ProductController.java
@@ -3,10 +3,13 @@ package com.sdia.tp2.web;
 
 import com.sdia.tp2.entities.Product;
 import com.sdia.tp2.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -29,6 +32,22 @@ public class ProductController {
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
         return "redirect:/index";
+    }
 
+    @GetMapping("/")
+    public String home(Model model){
+        return "redirect:/index";
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "new-product";}
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product , BindingResult bindingResult , Model model) {
+        if(bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/newProduct";
     }
 }
